85503fbb9cb45a4a5dd901260ef2b75462264c49
akash patel <akash906129@gmail.com>
2025-04-03 16:52:16 +0530

    added TOKEN_PROGRAM_2022

--- src/components/TokenLaunchpad.tsx
+++ src/components/TokenLaunchpad.tsx
@@ -1,14 +1,17 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
 import { useState } from "react";
 import {
-    createAssociatedTokenAccountInstruction, createInitializeMint2Instruction,
+    createAssociatedTokenAccountInstruction, createInitializeMetadataPointerInstruction, createInitializeMint2Instruction,
     createMintToInstruction,
+    ExtensionType,
     getAssociatedTokenAddressSync,
-    getMinimumBalanceForRentExemptMint,
-    MINT_SIZE,
-    TOKEN_PROGRAM_ID
+    getMintLen,
+    LENGTH_SIZE,
+    TOKEN_2022_PROGRAM_ID,
+    TYPE_SIZE
 } from '@solana/spl-token';
 import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { createInitializeInstruction, pack } from "@solana/spl-token-metadata";
 
 export function TokenLaunchpad() {
     const [name, setName] = useState<string>('');
@@ -19,9 +22,21 @@ export function TokenLaunchpad() {
     const { connection } = useConnection();
 
     async function createToken() {
-        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
         const keypair = Keypair.generate();
 
+        const metadata = {
+            mint: keypair.publicKey,
+            name: name,
+            symbol: symbol,
+            uri: imageUrl,
+            additionalMetadata: [],
+        };
+
+        const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+        const metadataLen = TYPE_SIZE + LENGTH_SIZE + pack(metadata).length;
+
+        const lamports = await connection.getMinimumBalanceForRentExemption(mintLen + metadataLen);
         if (wallet.publicKey) {
 
             const transaction = new Transaction();
@@ -31,17 +46,33 @@ export function TokenLaunchpad() {
                 SystemProgram.createAccount({
                     fromPubkey: wallet.publicKey,
                     newAccountPubkey: keypair.publicKey,
-                    space: MINT_SIZE,
+                    space: mintLen,
                     lamports,
-                    programId: TOKEN_PROGRAM_ID,
-                })
+                    programId: TOKEN_2022_PROGRAM_ID,
+                }),
+
+                createInitializeMetadataPointerInstruction(keypair.publicKey, wallet.publicKey, keypair.publicKey, TOKEN_2022_PROGRAM_ID),
+
             );
+
+
 
             console.log("1. mint account created");
 
             // Step 2: Initializing the mint
             transaction.add(
-                createInitializeMint2Instruction(keypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID)
+                createInitializeMint2Instruction(keypair.publicKey, 9, wallet.publicKey, wallet.publicKey, TOKEN_2022_PROGRAM_ID),
+
+                createInitializeInstruction({
+                    programId: TOKEN_2022_PROGRAM_ID,
+                    mint: keypair.publicKey,
+                    metadata: keypair.publicKey,
+                    name: metadata.name,
+                    symbol: metadata.symbol,
+                    uri: metadata.uri,
+                    mintAuthority: wallet.publicKey,
+                    updateAuthority: wallet.publicKey,
+                }),
 
             );
             console.log("2. Initializing the mint");
@@ -51,7 +82,7 @@ export function TokenLaunchpad() {
                 keypair.publicKey,
                 wallet.publicKey,
                 false,
-                TOKEN_PROGRAM_ID,
+                TOKEN_2022_PROGRAM_ID,
             );
             console.log("3. Creating Associated Token Account (ATA) : " + associatedToken);
 
@@ -61,7 +92,7 @@ export function TokenLaunchpad() {
                     associatedToken,
                     wallet.publicKey,
                     keypair.publicKey,
-                    TOKEN_PROGRAM_ID,
+                    TOKEN_2022_PROGRAM_ID,
                 )
             );
             console.log("3. (ATA) : finished");
@@ -69,7 +100,7 @@ export function TokenLaunchpad() {
             // Step 4: Minting tokens to the Associated Token Account
 
             transaction.add(
-                createMintToInstruction(keypair.publicKey, associatedToken, wallet.publicKey, initialSupply || 100000000000000, [], TOKEN_PROGRAM_ID)
+                createMintToInstruction(keypair.publicKey, associatedToken, wallet.publicKey, initialSupply || 100000000000000, [], TOKEN_2022_PROGRAM_ID)
 
             );
 
@@ -101,10 +132,10 @@ export function TokenLaunchpad() {
 
     return <div className="h-80vh flex justify-center items-center flex-col pb-10 pt-10">
         <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' value={name} placeholder='Name' onChange={(e) => { String(setName(e.target.value)) }}></input> <br />
-        <input className='inputText' type='text' value={symbol} placeholder='Symbol' onChange={(e) => { String(setSymbol(e.target.value)) }}></input> <br />
-        <input className='inputText' type='text' value={imageUrl} placeholder='Image URL' onChange={(e) => { String(setImageUrl(e.target.value)) }} ></input> <br />
-        <input className='inputText' type='text' value={initialSupply} placeholder='Initial Supply' onChange={(e) => { String(setInitialSupply(Number(e.target.value))) }}></input> <br />
+        <input className='inputText' type='text' value={name} placeholder='Name' onChange={(e) => { setName(e.target.value) }}></input> <br />
+        <input className='inputText' type='text' value={symbol} placeholder='Symbol' onChange={(e) => { setSymbol(e.target.value) }}></input> <br />
+        <input className='inputText' type='text' value={imageUrl} placeholder='Image URL' onChange={(e) => { setImageUrl(e.target.value) }} ></input> <br />
+        <input className='inputText' type='text' value={initialSupply} placeholder='Initial Supply' onChange={(e) => { setInitialSupply(Number(e.target.value)) }}></input> <br />
         <button className='btn' onClick={createToken}>Create a token</button>
     </div>
 }
